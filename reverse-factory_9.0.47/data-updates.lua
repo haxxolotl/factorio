3bf3ce47131bae3ac37f4bd91992e1b5a05178f6
Gene Liu <liuenting990305@gmail.com>
2026-03-22 00:15:47 -0700

    asd

--- reverse-factory_9.0.47/data-updates.lua
+++
@@ -1,7 +0,0 @@
--- Create the solid fluids during this step because earlier is better?
--- pymods breaks with this because it generates recipes during final-fixes for some reason
-if not mods["pypostprocessing"] then
-	require("prototypes.solid-fluids")
-end
--- Update the values for reverse factory technologies and recipes
-require("prototypes.reverse-factory-updates")
